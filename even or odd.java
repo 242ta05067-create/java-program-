b8b369375921cff179aa78c935d0c0a266d17078
242ta05067-create <242ta05067@ashokacollege.in>
2026-08-08 01:53:32 -0700

    Implement Even or Odd number checker
    Fix syntax errors and correct class and method definitions.

---
+++ even or odd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class EvenOdd(){
+  public static void main(sting args[]){
+    Scanner sc=new Scanner(System.in);
+    system.out.println("enter a number:");
+    int n=sc.nextInt();
+    if(n%2==0){
+      system.out.println("the number is even");
+    }else{
+      system.out.println("the number is odd");
+    }
+    sc.close();
+  }
+}
+
+
+
